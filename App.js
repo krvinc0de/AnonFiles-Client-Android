e7d27f226e6b3aa61272510aee5b9155efc985c8
Kevin Medina <79812923+krvinc0de@users.noreply.github.com>
2023-03-17 14:32:57 -0600

    nose
    nose

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
 
 export default function App() {
+
+  const uploadFile = async () => {
+    try {
+      const res = await DocumentPicker.getDocumentAsync({})
+      if (res.type === 'cancel') return;
+      console.log(res.uri);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require('./assets/anonlogo.png')}
+        style={styles.imageLogo}
+      />
+      <View style={styles.containerUpload}>
+        <Text style={styles.textPage}>Select file to upload</Text>
+        {
+          console.log('Hola mundo')
+        }
+        <TouchableOpacity style={styles.buttonFile}
+          onPress={uploadFile}
+        >
+          <Text style={styles.textPage}>Select File</Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +39,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    alignItems: 'center',
+    paddingTop: '20%'
+  },
+  containerUpload: {
+    width: '100%',
+    alignItems: 'center'
+  },
+  textPage: {
+    color: '#fff'
+  },
+  imageLogo: {
+    width: '80%',
+    height: 100,
+    marginBottom: 20
+  },
+  buttonFile: {
+    width: '60%',
+    height: 50,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    backgroundColor: 'grey',
+    borderRadius: 20,
+    marginTop: 20
+  }
 });
